863378f7dcce97b6304df048e6c9396fd481467a
2003platinum99-cell <2003.platinum99@gmail.com>
2025-09-05 01:54:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // IMPORTANT: Replace 'somali-programmer-ai-bot' with the name of your GitHub repository.
-const repositoryName = 'somali-programmer-ai-bot';
+const repositoryName = 'AI';
 
 // https://vitejs.dev/config/
 export default defineConfig({
